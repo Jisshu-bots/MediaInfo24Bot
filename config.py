37d603f8c346500e31745f4e504986bc05fb7e06
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-16 18:31:24 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

---
+++ config.py
@@ -0,0 +1,17 @@
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+import re
+from os import environ
+import os
+
+id_pattern = re.compile(r'^.\d+$')
+
+
+API_ID = int(os.environ.get("API_ID", "10811400"))
+API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAEUGww1KdtlQsMFERlA7elYlR82U-Nb6kk")
+ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+DOWNLOAD_LOCATION = "./DOWNLOADS"
+SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
